39ad37cfcdb4556c12f59a46dd9c7c51c0a42ece
Dansunn <102135172+Dansunn@users.noreply.github.com>
2023-03-17 19:16:17 +0700

    Update DummyStreamlit.py

--- DummyStreamlit.py
+++ DummyStreamlit.py
@@ -14,4 +14,4 @@ df = load_data(st.secrets["public_gsheets_url"])
 
 # Print results.
 for row in df.itertuples():
-    st.write(f"{row.name} has a :{row.team} has a;{row.number}:")
+    st.write(f"{row.name} bermain untuk {row.team} dengan nomor punggung {row.number}:")

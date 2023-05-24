86278d3f980ce2db6324dfc50ec8466ab7da0db0
Dansunn <102135172+Dansunn@users.noreply.github.com>
2023-05-24 20:39:51 +0700

    Update Home.py

--- UAS/Home.py
+++ UAS/Home.py
@@ -7,9 +7,6 @@ from htbuilder import HtmlElement, div, ul, li, br, hr, a, p, img, styles, class
 from htbuilder.units import percent, px
 from htbuilder.funcs import rgba, rgb
 
-if __name__ == "__main__":
-    main()
-
 def image(src_as_string, **style):
     return img(src=src_as_string, style=styles(**style))
 
